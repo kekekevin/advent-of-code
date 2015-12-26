10d540047c246da8ad018be82f4d621f43abd26a
Kevin Tung <kekekevin@gmail.com>
2015-12-26 00:12:02 -0600

    aoc day 1

---
+++ src/aoc/core.clj
@@ -0,0 +1,31 @@
+(ns aoc.core
+  (:require [clojure.string :refer [trim]])
+  (:gen-class))
+
+(defn move [c]
+  (if (= c \() 1 -1))
+
+(defn basement-position [input] 
+  (loop [position 1
+         floor 0
+         remaining input]
+    (if (= -1 (+ floor (move (first remaining))))
+      position
+      (recur (+ 1 position) 
+             (+ floor (move (first remaining)))
+             (rest remaining))
+    )))
+
+(defn aoc1-1 [input]
+  (reduce (fn [sum c]
+            (+ (move c) sum)) 
+          0 (seq input)))
+
+(defn aoc1-2 [input]
+  (basement-position input))
+
+(defn -main
+  [& args]
+  (println "1-1: " (aoc1-1 (trim (slurp "1.in"))))
+  (println "1-2: " (aoc1-2 (trim (slurp "1.in")))))
+
---
+++ test/aoc/core_test.clj
@@ -0,0 +1,14 @@
+(ns aoc.core-test
+  (:require [clojure.test :refer :all]
+            [aoc.core :refer :all]))
+
+(deftest aoc1-test
+  (testing "example 1"
+    (is (= 0 (aoc1 "()()")))
+    (is (= 0 (aoc1 "(())")))
+    )
+  (testing "example 2"
+    (is (= 3 (aoc1 "(((")))
+    (is (= 3 (aoc1 "(()(()(")))
+    )
+  )
